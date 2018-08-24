6a7a9b27218ecb8dc26b5b08aeaabd7350c40061
ferris <jake@fusetools.com>
2018-08-24 19:25:59 +0200

    Nybbles yo
    Lots of stuff here; better normalization, formalization of different contexts/encoding/decoding, nybble packing/unpacking, ...

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,138 @@ use std::fs::File;
 use std::io::Read;
 use std::iter;
 
+const ALPHABET_SIZE: u32 = 16; // Only bits for now
+
+// Scaled probability sum (determines # of states, coding precision)
+const M: u32 = 128;
+
+// Renormalization range
+const K: u32 = 1; // Scaling factor (grows # of states, coding precision by a constant factor)
+const L: u32 = M * K;
+
+// State count
+const STATE_COUNT: u32 = L * 2;
+
+struct Context {
+    encoding_table: Vec<u32>,
+    decoding_table: Vec<(u32, u32)>,
+    symbol_precursor_ranges: Vec<(u32, u32)>,
+}
+
+impl Context {
+    fn new(symbol_frequencies: &[u32]) -> Context {
+        println!("Symbol frequencies: {:?}", symbol_frequencies);
+
+        let total_symbols = symbol_frequencies.iter().fold(0, |acc, x| acc + x);
+
+        // Symbol probabilities (approx.)
+        let symbol_probabilities =
+            symbol_frequencies.iter()
+            .map(|f| (*f as f64) / (total_symbols as f64))
+            .collect::<Vec<_>>();
+
+        //println!("Symbol probabilities: {:?}", symbol_probabilities);
+
+        // Scaled integer probabilities (must sum to L)
+        let mut scaled_symbol_probabilities =
+            symbol_probabilities.iter()
+            .map(|p| (*p * (L as f64)).ceil() as u32)
+            .collect::<Vec<_>>();
+        //  Due to rounding errors, we may have to adjust the scaled probabilities a bit so they sum to L.
+        //  We do this by iteratively subtracting 1 from the probability that will suffer the least by doing so.
+        while scaled_symbol_probabilities.iter().fold(0, |acc, x| acc + *x) > L {
+            let adjust_index = scaled_symbol_probabilities.iter().enumerate().fold(None, |acc: Option<(usize, f64)>, (index, x)| {
+                if *x <= 1 {
+                    acc
+                } else {
+                    let adjusted_x = *x - 1;
+                    let adjust_error = ((*x as f64) / (adjusted_x as f64)).log2();
+                    match acc {
+                        Some(acc) => {
+                            if adjust_error < acc.1 {
+                                Some((index, adjust_error))
+                            } else {
+                                Some(acc)
+                            }
+                        }
+                        _ => Some((index, adjust_error))
+                    }
+                }
+            }).unwrap().0;
+            scaled_symbol_probabilities[adjust_index] -= 1;
+        }
+
+        println!("Scaled symbol probabilities: {:?}", scaled_symbol_probabilities);
+
+        // Precursor ranges
+        let symbol_precursor_ranges =
+            scaled_symbol_probabilities.iter()
+            .map(|p| (*p, *p * 2 - 1))
+            .collect::<Vec<_>>();
+
+        //println!("Symbol precursor ranges (Is, inclusive): {:?}", symbol_precursor_ranges);
+
+        // Sorted symbols (the simplest/most naive construction, not necessarily worst, but likely to be due to precision loss)
+        /*let sorted_symbols =
+            scaled_symbol_probabilities.iter()
+            .enumerate()
+            .flat_map(|(s, p)| iter::repeat(s as u32).take(*p as _))
+            .collect::<Vec<_>>();*/
+
+        let sorted_symbols = {
+            const UNALLOCATED: u32 = ALPHABET_SIZE;
+            let mut ret = vec![UNALLOCATED; L as usize];
+            let mut target_index = 0;
+            for (symbol, probability) in scaled_symbol_probabilities.iter().enumerate() {
+                for _ in 0..*probability {
+                    while ret[target_index as usize] != UNALLOCATED {
+                        target_index = (target_index + 1) & (L - 1);
+                    }
+
+                    ret[target_index as usize] = symbol as _;
+                    target_index = (target_index + 37) & (L - 1);
+                }
+            }
+            ret
+        };
+
+        println!("Sorted symbols: {}", sorted_symbols.iter().fold(String::new(), |acc, x| format!("{}{:1x}", acc, *x)));
+
+        // Encoding/decoding table/string (the simplest/most naive construction as sparse 2D arrays, basically the worst possible option but most intuitive)
+        let mut encoding_table = vec![0; (STATE_COUNT * ALPHABET_SIZE) as usize];
+        let mut decoding_table = vec![(0, 0); STATE_COUNT as usize];
+
+        let mut symbol_max_states =
+            scaled_symbol_probabilities.iter()
+            .map(|p| *p - 1) // - 1 because we want this to be an inclusive upper bound
+            .collect::<Vec<_>>();
+
+        for to_state in L..L * 2 {
+            let symbol = sorted_symbols[(to_state - L) as usize];
+
+            symbol_max_states[symbol as usize] += 1;
+            let from_state = symbol_max_states[symbol as usize];
+
+            encoding_table[(from_state * ALPHABET_SIZE + symbol) as usize] = to_state;
+
+            decoding_table[to_state as usize] = (symbol, from_state);
+        }
+
+        //println!("Encoding table: {:?}", encoding_table);
+        //println!("Decoding table: {:?}", decoding_table);
+
+        Context {
+            encoding_table: encoding_table,
+            decoding_table: decoding_table,
+            symbol_precursor_ranges: symbol_precursor_ranges,
+        }
+    }
+}
+
 fn main() {
     let input_file_name = args().nth(1).expect("Couldn't read input file name arg");
 
     // Input symbols
-    const ALPHABET_SIZE: u32 = 256; // Only bits for now
     //let input = vec![0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // Unpacked, raw symbols
     let input = {
         let mut file = File::open(input_file_name).expect("Couldn't open input file");
@@ -18,115 +145,31 @@ fn main() {
 
     println!("Alphabet size: {}", ALPHABET_SIZE);
     //println!("Input string: {:?} ({} bits)", input, input.len() * 8);
-
-    // Symbol frequencies
-    let symbol_frequencies =
-        (0..ALPHABET_SIZE)
-        .map(|s| input.iter().fold(0, |acc, x| if *x == s { acc + 1 } else { acc }))
-        .collect::<Vec<_>>();
-
-    println!("Symbol frequencies: {:?}", symbol_frequencies);
-
-    // Symbol probabilities (approx.)
-    let symbol_probabilities =
-        symbol_frequencies.iter()
-        .map(|f| (*f as f64) / (input.len() as f64))
-        .collect::<Vec<_>>();
-
-    println!("Symbol probabilities: {:?}", symbol_probabilities);
-
-    // Scaled probability sum (determines # of states, coding precision)
-    const M: u32 = 4096;
+    println!("Input size: {} bits ({} bytes)", input.len() * 8, input.len());
 
     println!("M: {}", M);
-
-    // Renormalization range
-    const K: u32 = 1; // Scaling factor (grows # of states, coding precision by a constant factor)
-    const L: u32 = M * K;
 
     println!("K: {}", K);
     println!("L: {}", L);
     println!("Renormalization range (I) = [{}, {}]", L, L * 2 - 1);
 
-    // Scaled integer probabilities (must sum to L)
-    //  Note that in very extreme cases (eg. a symbol has ~0 probability) this algorithm might fail. :)
-    let mut scaled_symbol_probabilities =
-        symbol_probabilities.iter()
-        .map(|p| {
-            let mut ret = (*p * (L as f64)).floor() as u32;
-            if ret < 1 {
-                ret = 1;
-            }
-            ret
-        })
-        .collect::<Vec<_>>();
-    //  Due to rounding errors, we may have to adjust the scaled probabilities a bit so they sum to L.
-    //  Adjusting the last prob is the easiest way to do this, although it's likely not very accurate.
-    while scaled_symbol_probabilities.iter().fold(0, |acc, x| acc + *x) < L {
-        let i = scaled_symbol_probabilities.len() - 1;
-        scaled_symbol_probabilities[i] += 1;
-    }
-
-    //println!("Scaled symbol probabilities: {:?}", scaled_symbol_probabilities);
-
-    // Cumulative scaled probalities
-    let cumulative_scaled_symbol_probabilities =
-        scaled_symbol_probabilities.iter()
-        .scan(0, |acc, x| {
-            let ret = *acc;
-
-            *acc += *x;
-
-            Some(ret)
-        })
-        .collect::<Vec<_>>();
-
-    //println!("Cumulative scaled symbol probabilities: {:?}", cumulative_scaled_symbol_probabilities);
-
-    // Precursor ranges
-    let symbol_precursor_ranges =
-        scaled_symbol_probabilities.iter()
-        .map(|p| (*p, *p * 2 - 1))
-        .collect::<Vec<_>>();
-
-    //println!("Symbol precursor ranges (Is, inclusive): {:?}", symbol_precursor_ranges);
-
-    // Sorted symbols (the simplest/most naive construction, not necessarily worst, but likely to be due to precision loss)
-    let sorted_symbols =
-        scaled_symbol_probabilities.iter()
-        .enumerate()
-        .flat_map(|(s, p)| iter::repeat(s as u32).take(*p as _))
-        .collect::<Vec<_>>();
-
-    //println!("Sorted symbols: {}", sorted_symbols.iter().fold(String::new(), |acc, x| format!("{}{}", acc, *x)));
-
-    // State count
-    const STATE_COUNT: u32 = L * 2;
-
     println!("State count: {}", STATE_COUNT);
 
-    // Encoding/decoding table/string (the simplest/most naive construction as sparse 2D arrays, basically the worst possible option but most intuitive)
-    let mut encoding_table = vec![0; (STATE_COUNT * ALPHABET_SIZE) as usize];
-    let mut decoding_table = vec![(0, 0); STATE_COUNT as usize];
+    let mut first_nybble_symbol_frequencies = vec![0; 16];
+    let mut second_nybble_symbol_frequencies = vec![vec![0; 16]; 16];
 
-    let mut symbol_max_states =
-        scaled_symbol_probabilities.iter()
-        .map(|p| *p - 1) // - 1 because we want this to be an inclusive upper bound
-        .collect::<Vec<_>>();
+    for byte in input.iter() {
+        let byte = *byte;
 
-    for to_state in L..L * 2 {
-        let symbol = sorted_symbols[(to_state - L) as usize];
+        let first_nybble = byte >> 4;
+        let second_nybble = byte & 0x0f;
 
-        symbol_max_states[symbol as usize] += 1;
-        let from_state = symbol_max_states[symbol as usize];
-
-        encoding_table[(from_state * ALPHABET_SIZE + symbol) as usize] = to_state;
-
-        decoding_table[to_state as usize] = (symbol, from_state);
+        first_nybble_symbol_frequencies[first_nybble as usize] += 1;
+        second_nybble_symbol_frequencies[first_nybble as usize][second_nybble as usize] += 1;
     }
 
-    //println!("Encoding table: {:?}", encoding_table);
-    //println!("Decoding table: {:?}", decoding_table);
+    let first_nybble_context = Context::new(&first_nybble_symbol_frequencies);
+    let second_nybble_contexts = second_nybble_symbol_frequencies.iter().map(|f| Context::new(f)).collect::<Vec<_>>();
 
     // Encoding
     let mut encoded_string = Vec::new(); // Note that this is LIFO, not FIFO
@@ -139,25 +182,19 @@ fn main() {
         // Input symbol
         let symbol = *symbol;
 
-        // Stream out bits from state until we're in the current symbol's precursor range (I -> Is)
-        let precursor_range_end = symbol_precursor_ranges[symbol as usize].1;
+        let first_nybble = symbol >> 4;
+        let second_nybble = symbol & 0x0f;
 
-        while state > precursor_range_end {
-            let output_bit = state & 0x01;
-            encoded_string.push(output_bit);
-            state >>= 1;
-        }
-
-        // Update state
-        let next_state_index = state * ALPHABET_SIZE + symbol;
-        state = encoding_table[next_state_index as usize];
+        state = encode(second_nybble, state, &second_nybble_contexts[first_nybble as usize], &mut encoded_string);
+        state = encode(first_nybble, state, &first_nybble_context, &mut encoded_string);
     }
 
     let final_state = state;
 
     println!("Final encoding state (x'): {}", final_state);
     //println!("Encoded string: {:?} ({} bits)", encoded_string, encoded_string.len());
-    println!("Encoded string size: {} bits", encoded_string.len());
+    let ratio = (1.0 - (encoded_string.len() as f64) / ((input.len() * 8) as f64)) * 100.0;
+    println!("Encoded string size: {} bits (~{} bytes, {:.*}%)", encoded_string.len(), encoded_string.len() / 8, 2, ratio);
 
     // Decoding
     let mut decoded_string = Vec::new();
@@ -167,32 +204,60 @@ fn main() {
     println!("Initial decoding state (x): {}", state);
 
     for _ in 0..input.len() {
-        // Look up symbol and next state base in table
-        let (symbol, next_state_base) = decoding_table[state as usize];
+        let (first_nybble, new_state) = decode(state, &first_nybble_context, &mut encoded_string);
+        state = new_state;
+        let (second_nybble, new_state) = decode(state, &second_nybble_contexts[first_nybble as usize], &mut encoded_string);
+        state = new_state;
+
+        let symbol = (first_nybble << 4) | second_nybble;
 
         // Output symbol
         decoded_string.push(symbol);
-
-        // Update state
-        state = next_state_base;
-
-        // Pull in bits until state is in renormalization range (Is -> I, should mirror encoding output bits to move from I -> Is)
-        while state < L {
-            let input_bit = encoded_string.pop().unwrap();
-            state <<= 1;
-            state |= input_bit;
-        }
     }
 
     let final_state = state;
 
     println!("Final decoding state (x'): {}", final_state);
     //println!("Decoded string: {:?} ({} bits)", decoded_string, decoded_string.len() * 8);
-    println!("Decoded string size: {} bits", decoded_string.len() * 8);
+    println!("Decoded string size: {} bits ({} bytes)", decoded_string.len() * 8, decoded_string.len());
 
     if decoded_string == input {
         println!("Input/output match!");
     } else {
         println!("Input/output don't match :(");
     }
+}
+
+fn encode(symbol: u32, mut state: u32, context: &Context, encoded_string: &mut Vec<u32>) -> u32 {
+    // Stream out bits from state until we're in the current symbol's precursor range (I -> Is)
+    let precursor_range_end = context.symbol_precursor_ranges[symbol as usize].1;
+
+    while state > precursor_range_end {
+        let output_bit = state & 0x01;
+        encoded_string.push(output_bit);
+        state >>= 1;
+    }
+
+    // Update state
+    let next_state_index = state * ALPHABET_SIZE + symbol;
+    state = context.encoding_table[next_state_index as usize];
+
+    state
+}
+
+fn decode(mut state: u32, context: &Context, encoded_string: &mut Vec<u32>) -> (u32, u32) {
+    // Look up symbol and next state base in table
+    let (symbol, next_state_base) = context.decoding_table[state as usize];
+
+    // Update state
+    state = next_state_base;
+
+    // Pull in bits until state is in renormalization range (Is -> I, should mirror encoding output bits to move from I -> Is)
+    while state < L {
+        let input_bit = encoded_string.pop().unwrap();
+        state <<= 1;
+        state |= input_bit;
+    }
+
+    (symbol, state)
 }
